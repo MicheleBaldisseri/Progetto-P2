b81ccfe54dcf531cecc8e481391c1ea1691fb86c
Matthew <matthewb487@gmail.com>
2020-04-17 00:30:16 +0200

    Dichiarazioni su Evento (con errori ovvi nelle derivate)

--- evento.h
+++ evento.h
@@ -1,10 +1,31 @@
 #ifndef EVENTO_H
 #define EVENTO_H
-#include "data.h"
+#include <iostream>
+#include "dataora.h"
+using std::string;
+
+enum Color  {white, red, green, yellow, orange, black, blue, purple, grey};
 
 class Evento{
+private:
+    string titolo;
+    dataora dataInizio;
+    Color colore;
 public:
     Evento();
+    Evento(string, dataora, Color=white);//costruttore per classi derivate
+
+    string descrizione() const = 0;
+
+    Color setColore() const = 0;
+    Color getColore() const;
+    string getTitolo() const;
+    dataora getDataora() const;
+
+    virtual bool operator==(const Evento& e) const;
+
+    virtual ~Evento(){}
+    virtual Evento* clone() const = 0;
 };
 
 #endif // EVENTO_H
