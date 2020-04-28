920605278bf46302995065c548bd73e66cb82340
zezze28 <62375818+zezze28@users.noreply.github.com>
2020-04-28 11:41:45 +0200

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include "dataora.h"
 #include "evento.h"
+#include "lista.h"
 using namespace std;
-template <class T> class lista;
-template <class T> ostream& operator<<(ostream&, const lista<T>&);
 
-template <class T=Evento>
+class const_iterator;
+
+
+template <class T>
 class lista{
-	friend class constiterator;
-	friend ostream& operator<< <T>(ostream&, const lista<T>&);
 private:
 	class nodo{
 	public:
 		T info;
 		nodo* next;
 		nodo(const T& t=T(),nodo* n=0): info(t), next(n){}
-		~nodo(){delete next;} 
+		~nodo(){delete next;}
 	};
 	nodo* first, *last;
+public:
 	static bool isBefore(nodo* a, nodo* b){
 		if(!a) return true;
 		if(!b) return false;
-		return a->info->data < b->info->data;
+		return a->info/*->data*/ < b->info/*->data*/;
 	}
 	static nodo* copy(nodo* a, nodo*& b){
 		if(!a) return 0;
@@ -31,7 +32,6 @@ private:
 		}
 		else return new nodo(a->info, copy(a->next,b));
 	}
-public:
 	//lista(const T& t) costruisce una lista contenente l’elemento t
 	lista(const T& t=T()): first(0), last(0){insert(t);}
 	//costruttore copia profonda
@@ -46,7 +46,7 @@ public:
 	}
 	//distruttore
 	~lista(){delete first; delete last;}
-	
+
 		void insert(const T& t){
 		nodo* nuovo;
 		if(!first){
@@ -66,7 +66,7 @@ public:
 				last=nuovo;
 			}
 			nodo* prec=first;
-			for(const_iterator cit=begin()->next; cit!=end(); ++cit){
+			for(const_iterator cit=cit.begin()->next; cit!=cit.end(); ++cit){
 				if(nuovo<cit){
 					prec->next=nuovo;
 					nuovo->next=cit;
@@ -78,45 +78,42 @@ public:
 	bool operator<(const lista& c) const {
 		return isBefore(first,c.first);
 	}
-}
-	
-class const_iterator{
-	public:
-		nodo* punt;
-		//operator++ prefisso
-		const_iterator& operator++() {
-			if (punt)
-				punt = punt->next;
-			return *this;
-		}
-		
-		const_iterator operator++(int) {
-			const_iterator aux = *this;
-			if (punt)
-				punt = punt->next;
+	class const_iterator{
+		public:
+			nodo* punt;
+			//operator++ prefisso
+			const_iterator& operator++() {
+				if (punt)
+					punt = punt->next;
+				return *this;
+			}
+
+			const_iterator operator++(int) {
+				const_iterator aux = *this;
+				if (punt)
+					punt = punt->next;
+				return aux;
+			}
+			bool operator==(const const_iterator& x) const {
+			  return punt==x.punt;
+			}
+
+			bool operator!=(const const_iterator& x) const {
+			  return punt!=x.punt;
+			}
+			const T& operator*() const {
+			  return punt->info;
+			}
+		const_iterator begin() const {
+			const_iterator aux;
+			aux.punt = first;
 			return aux;
 		}
-		bool operator==(const const_iterator& x) const {
-		  return punt==x.punt;
-		}
 
-		bool operator!=(const const_iterator& x) const {
-		  return punt!=x.punt;
+		const_iterator end() const {
+			const_iterator aux;
+			aux.punt = last;
+			return aux;
 		}
-		const T& operator*() const {
-		  return punt->info;
-		}
-	 };
-
-	const_iterator begin() const {
-		const_iterator aux;
-		aux.punt = first;
-		return aux;
-	}
-
-	const_iterator end() const {
-		const_iterator aux;
-		aux.punt = last;
-		return aux;
-	}
+	};
 };
